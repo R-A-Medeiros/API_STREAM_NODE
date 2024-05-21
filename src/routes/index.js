576b9338e4a0982663033665808fe50af64fc8c4
R-A-Medeiros <r.medeiros1206@gmail.com>
2024-05-20 22:46:28 -0300

    Adição do middlewere helmet

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import filmes from "./filmesRoutes.js";
 import series from "./seriesRoutes.js";
 import usuarios from "./usuarioRoutes.js";
+import helmet from "helmet";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send("API Stream"));
 
-    app.use(express.json(), filmes, series, usuarios)
+    app.use(express.json(), helmet(), filmes, series, usuarios)
 };
 
 export default routes;
